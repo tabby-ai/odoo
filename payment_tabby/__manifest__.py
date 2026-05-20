4309225e4317026401b17775a3c032a01a2f6a5f
Serhii Balitskyi <sergey.balitsky@gmail.com>
2026-05-20 12:27:04 +0000

    Version up

--- payment_tabby/__manifest__.py
+++ payment_tabby/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Payment Provider: Tabby",
-    'version': '2.0.4',
+    'version': '2.0.5',
     'category': 'Accounting/Payment Providers',
     'sequence': 350,
     'summary': "Tabby payment provider",
