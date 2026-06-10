19dce823c26236761084e29fe2d94fe1717fa40f
Serhii Balitskyi <sergey.balitsky@gmail.com>
2026-06-10 13:00:42 +0000

    Additional fixes for non standard payment providers

--- payment_tabby/__manifest__.py
+++ payment_tabby/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Payment Provider: Tabby",
-    'version': '2.0.8',
+    'version': '2.0.9',
     'category': 'Accounting/Payment Providers',
     'sequence': 350,
     'summary': "Tabby payment provider",
